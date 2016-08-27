99c65e3543a969ad17740809ba823962a099f90a
Richard <richard1248c@gmail.com>
2016-08-27 16:24:22 -0400

    Update dice.py

--- Dice/dice.py
+++ Dice/dice.py
@@ -4,7 +4,7 @@ roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
     die = int(input("How many die?\n"))
-    print ("Rolling the die...")
+    print ("Rolling the dice...")
     print ("The values are....")
     for num in range(0, die):
         print (random.randint(1, 6))
